6e2f9278fa6e71b8bbdc8e3a1335f45a109c1300
Ryan Peden <ryan04@gmail.com>
2019-02-12 11:51:45 -0500

    moved books into src directory; added books to app state

--- step3/sales-dash-react/src/App.js
+++ step3/sales-dash-react/src/App.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
-import logo from './logo.svg';
 import './App.css';
 
 import 'wijmo/styles/wijmo.css';
 import { CollectionView } from 'wijmo/wijmo';
 import { FlexGrid } from 'wijmo/wijmo.react.grid';
 
+import { getBooks } from './data/book';
+
 class App extends Component {
 	constructor(props) {
 		super(props);
-		const bookGrid = document.getElementById('bookGrid');
-		ReactDOM.render(<BookGrid />, bookGrid);
+
+		this.state = {
+			books: getBooks()
+		};
 	}
-	componentDidMount() {
-		alert("argh!");
-	}
-	render(){
-		return(null);
+
+	render() {
+		return (
+			<div>
+				<BookGrid books={this.state.books} />
+			</div>
+		);
 	}
 }
 
 class BookGrid extends React.Component {
   render() {
     return (
-      <FlexGrid 
-		  itemsSource={window.getBooks()}
-		  loadedRows={function(s, e) { s.autoSizeColumns();}}
-		  rowEditEnded={function(s, e) { s.autoSizeColumns();}}
-
-	  />
+			<>
+				<FlexGrid
+					itemsSource={this.props.books}
+				/>
+			</>
     );
   }
 }
