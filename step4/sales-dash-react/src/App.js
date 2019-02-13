1dffec269506e6a6e0e6e1f73cf3e366d0aa5c3a
raddevus <1radeutsch1@gmail.com>
2019-02-13 16:53:15 -0500

    Final changes for Step4 completed code.

--- step4/sales-dash-react/src/App.js
+++ step4/sales-dash-react/src/App.js
@@ -6,7 +6,7 @@ import { CollectionView } from 'wijmo/wijmo';
 import { FlexGrid } from 'wijmo/wijmo.react.grid';
 
 import { helper } from './code/helper';
-import { getAuthor } from './data/author';
+import { getAuthors } from './data/author';
 import { getBooks } from './data/book';
 import { getBookStores } from './data/bookstore'
 import { getInventory } from './data/inventory'
@@ -19,14 +19,36 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			books: getBooks()
+			dto: this.buildDTO(),
+			authors: getAuthors()
 		};
+	}
+	
+	buildDTO() {
+		var books = getBooks();
+		var authors = getAuthors();
+		var dto = [];
+		for (var i = 0; i < books.length; i++) {
+		  dto.push({
+			title: books[i].title,
+			authorName: new helper().selectItemValuesById("name", authors, "id",books[i].authorId)[0],
+			genre: books[i].genre,
+			printCost: books[i].printCost,
+		  });
+		}
+		console.log(dto);
+		return dto;//new CollectionView(dto);
 	}
 	
 	render() {
 		return (
-			<div>
-				<BookGrid books={this.state.books} />
+			<div >
+				<div id="adjustL">
+				<BookGrid  books={this.state.dto} />
+				</div>
+				<div id="adjustR">
+				<AuthorGrid authors={this.state.authors} />
+				</div>
 			</div>
 		);
 	}
@@ -41,5 +63,16 @@ class BookGrid extends React.Component {
     );
   }
 }
+
+class AuthorGrid extends React.Component {
+  render() {
+    return (
+			<FlexGrid
+				itemsSource={this.props.authors}
+			/>
+    );
+  }
+}
+
 
 export default App;
