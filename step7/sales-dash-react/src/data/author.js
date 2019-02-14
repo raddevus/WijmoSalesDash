f0a894ce53b2dd660771be13587ee16bd1ac6ff3
raddevus <1radeutsch1@gmail.com>
2019-02-14 10:21:39 -0500

    Initial creation of step7

---
+++ step7/sales-dash-react/src/data/author.js
@@ -0,0 +1,11 @@
+//author.json
+// Author {"id":,"name":""}
+export function getAuthors() {
+	var allAuthors = [
+	{"id":1,"name":"Grisham,John"},
+	{"id":2,"name":"Baldacci,David"},
+	{"id":3,"name":"Cussler,Clive"},
+	{"id":4,"name":"Steele,Danielle"}
+	];
+	return allAuthors;
+}
---
+++ step7/sales-dash-react/src/data/salesrep.js
@@ -0,0 +1,11 @@
+//salesrep.json
+// {id:,name:"",location:""}
+export function getSalesReps(){
+	var allSalesReps = [
+		{id:1,name:"Fudd,Elmer",location:"Dallas"},
+		{id:2,name:"Flintstone,Fred",location:"Bedrock"},
+		{id:3,name:"Rubble,Barney",location:"Limestone City"},
+		{id:4,name:"Flintstone,Wilma",location:"Bedrock"}
+	];
+	return allSalesReps;
+}
