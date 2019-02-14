bffa614b86d89c23ac287edd29fae9979d4817fe
raddevus <1radeutsch1@gmail.com>
2019-02-14 09:01:15 -0500

    Initial creation of Step6

---
+++ step6/sales-dash-react/src/App.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import 'wijmo/styles/wijmo.css';
+import { CollectionView } from 'wijmo/wijmo';
+import { FlexGrid } from 'wijmo/wijmo.react.grid';
+import { FlexChart } from 'wijmo/wijmo.react.chart';
+
+import { helper } from './code/helper';
+import { getAuthors } from './data/author';
+import { getBooks } from './data/book';
+import { getBookStores } from './data/bookstore'
+import { getInventory } from './data/inventory'
+import { getLineItems } from './data/invoiceLineItem'
+import { getSalesReps } from './data/salesrep'
+import { getWarehouses } from './data/warehouse'
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			dto: this.buildDTO(),
+			authors: this.buildDTO(),
+			data:this.getChartData()
+		};
+		window.chartData = this.getChartData();	
+	}
+	
+	buildDTO() {
+		var books = getBooks();
+		var authors = getAuthors();
+		var dto = [];
+		for (var i = 0; i < books.length; i++) {
+		  dto.push({
+			title: books[i].title,
+			authorName: new helper().selectItemValuesById("name", authors, "id",books[i].authorId)[0],
+			genre: books[i].genre,
+			printCost: books[i].printCost,
+		  });
+		}
+		console.log(dto);
+		return dto;
+	}
+	
+	getChartData(){
+	  var h = new helper();
+	  var bookstores = h.selectItems("name",getBookStores());
+      var data = [];
+		for (var i = 0; i < bookstores.length; i++) {
+			data.push({
+			  bookstore: bookstores[i].name + "("+ bookstores[i].location + ")",
+			  sales: this.calculateSalesAndQuantityByStore(h,bookstores[i].id)[0],
+			  quantity: this.calculateSalesAndQuantityByStore(h,bookstores[i].id)[1],
+			  expenses: this.calculateCostOfGoodsSold(h,bookstores[i].id)
+			});
+		}
+		return data;
+	}
+	
+	calculateCostOfGoodsSold(helper,storeId){
+		var lineItems = helper.selectItemsById(getLineItems(), "bookstoreId", storeId);
+		var bookCost = 0;
+		for (var itemCount = 0; itemCount < lineItems.length;itemCount++){
+			bookCost += helper.selectItemValuesById("printCost", getBooks(), "id", lineItems[itemCount].bookId) * lineItems[itemCount].quantity;
+		}
+		return bookCost;
+	}
+	
+	calculateSalesAndQuantityByStore(helper,storeId){
+		// returns array of two values 
+		// [0] is totalSales
+		// [1] is quantity sold
+		console.log(storeId);
+		var lineItems = helper.selectItemsById(getLineItems(), "bookstoreId", storeId);
+		console.log(lineItems);
+		var totalStoreSales = 0;
+		var totalStoreQuantity = 0;
+		for (var itemCount = 0; itemCount < lineItems.length;itemCount++){
+			totalStoreSales += lineItems[itemCount].quantity * lineItems[itemCount].salePriceEach;
+			totalStoreQuantity += lineItems[itemCount].quantity;
+		}
+		var salesAndQuantity = [];
+		salesAndQuantity.push(totalStoreSales);
+		salesAndQuantity.push(totalStoreQuantity);
+		return salesAndQuantity;
+	}
+	
+	render() {
+		return (
+			<div >
+				<div id="adjustL">
+				<BookGrid  books={this.state.dto} />
+				</div>
+				<div id="adjustR">
+				<SalesChart data={this.state.data} />
+				</div>
+			</div>
+		);
+	}
+}
+
+class BookGrid extends React.Component {
+  render() {
+    return (
+			<FlexGrid
+				itemsSource={this.props.books}
+			/>
+    );
+  }
+}
+
+class SalesChart extends React.Component {
+  render() {
+    return (
+			<FlexChart
+				itemsSource={this.props.data}
+				bindingX={'bookstore'}
+				series={[
+					{ binding: 'sales', name: 'Sales' },
+				  { binding: 'expenses', name: 'Cost of Goods' },
+				  { binding: 'quantity', name: 'Books Sold' }
+				]}
+			/>
+    );
+  }
+}
+
+
+export default App;
---
+++ step6/sales-dash-react/src/data/invoiceLineItem.js
@@ -0,0 +1,14 @@
+//invoiceLineItem
+// {id:,saleDate:,quantity:,salePriceEach:,salesRepId:,bookstoreId:,bookId:,warehouseId:}
+export function getLineItems(){
+	var allLineItems = [
+		{id:1,saleDate:"2018-05-01",quantity:300,salePriceEach:2.50,salesRepId:1,bookstoreId:1,bookId:1,warehouseId:1},
+		{id:2,saleDate:"2018-05-01",quantity:370,salePriceEach:3.25,salesRepId:1,bookstoreId:1,bookId:1,warehouseId:2},
+		{id:3,saleDate:"2018-05-01",quantity:480,salePriceEach:1.75,salesRepId:1,bookstoreId:2,bookId:1,warehouseId:3},
+		{id:4,saleDate:"2018-05-02",quantity:220,salePriceEach:2.21,salesRepId:1,bookstoreId:3,bookId:1,warehouseId:3},
+		{id:5,saleDate:"2018-05-01",quantity:250,salePriceEach:1.75,salesRepId:1,bookstoreId:3,bookId:1,warehouseId:4},
+		{id:6,saleDate:"2018-05-04",quantity:250,salePriceEach:1.75,salesRepId:3,bookstoreId:4,bookId:1,warehouseId:4},
+		{id:7,saleDate:"2018-05-05",quantity:250,salePriceEach:1.75,salesRepId:2,bookstoreId:4,bookId:1,warehouseId:4}
+	];
+	return allLineItems;
+}
